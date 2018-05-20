887d51252d467c6e8849a259810a9121b23262a8
Alex Koch <alex549us3@gmail.com>
2018-05-20 16:12:52 +0000

    adds gitignore and fixes typo

--- storage/main.tf
+++ storage/main.tf
@@ -9,7 +9,7 @@ resource "random_id" "tf_bucket_id" {
 resource "aws_s3_bucket" "tf_code" {
   bucket = "${var.project_name}-${random_id.tf_bucket_id.dec}"
   acl = "private"
-  force_restroy = true
+  force_destroy = true
 
   tags {
     name = "tf_bucket"

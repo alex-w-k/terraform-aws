53a2183c01829470f56759a65bb6159de2af68e7
Alex Koch <alex549us3@gmail.com>
2018-05-21 00:07:29 +0000

    fixing syntax

--- networking/main.tf
+++ networking/main.tf
@@ -44,7 +44,7 @@ resource "aws_subnet" "tf_public_subnet" {
   vpc_id = "${aws_vpc.tf_vpc.id}"
   cidr_block = "${var.public_cidrs[count.index]}"
   map_public_ip_on_launch = true
-  availability_zone = "${data.aws_availability_zones.available_names[count.index]}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 
   tags {
     Name = "tf_public_${count.index + 1}"

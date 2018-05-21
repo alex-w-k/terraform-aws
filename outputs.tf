9dd0e74ac546a842678d23b9a978f48127473345
Alex Koch <alex549us3@gmail.com>
2018-05-21 22:20:18 +0000

    added some networking outputs to the main outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
+
+#----- Storage Outputs -----
 output "Bucket Name" {
   value = "${module.storage.bucketname}"
 }
+
+#----- Networking Outputs -----
+output "Public Subnets" {
+    value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "Subnet IPs" {
+    value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+output "Public Security Group" {
+    value = "${module.networking.public_sg}"
+}
+#----- Compute Outputs -----
